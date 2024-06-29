39e8f0941b3e50ed6fe5fbddfb7be6a3123fba1a
pthai <pthai.it.210501@gmail.com>
2024-06-29 08:42:31 +0700

    test: update tests for feedback service.

--- tests/feedback_tests/feedback_test.go
+++ tests/feedback_tests/feedback_test.go
@@ -2,68 +2,94 @@ package feedback_tests
 
 import (
 	"ESS-microservices/core/controllers"
+	"ESS-microservices/core/models"
 	"ESS-microservices/core/repositories"
 	"ESS-microservices/core/routes"
 	"ESS-microservices/database"
+	"ESS-microservices/database/drivers"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
-//func TestCreateFeedback(t *testing.T) {
-//	feedback := models.Feedback{
-//		Data: models.FeedbackData{
-//			Title:   "test title",
-//			Content: "test content",
-//		},
-//		Type:      1,
-//		IsBug:     true,
-//		AccountId: 1,
-//	}
-//
-//	router := gin.Default()
-//	routes.NewApiRoutes(router)
-//
-//	feedbackJson, _ := json.Marshal(feedback)
-//
-//	w := httptest.NewRecorder()
-//	request, _ := http.NewRequest("POST", "/api/v1/feedback", strings.NewReader(string(feedbackJson)))
-//	request.Header.Set("Content-Type", "application/json")
-//
-//	router.ServeHTTP(w, request)
-//
-//	fmt.Println(w.Body.String())
-//	assert.Equal(t, 201, w.Code)
-//}
+func TestCreateFeedback(t *testing.T) {
+	databaseConnection := database.Connection{
+		Driver: &drivers.PostgresDriver{},
+	}
+	databaseConnection.Connect()
 
-//func TestCreateFeedbackValidationError(t *testing.T) {
-//	feedback := models.Feedback{
-//		Data: models.FeedbackData{
-//			Title: "test title",
-//		},
-//		Type:      1,
-//		IsBug:     true,
-//		AccountId: 1,
-//	}
-//
-//	router := gin.Default()
-//	routes.NewApiRoutes(router)
-//
-//	feedbackJson, _ := json.Marshal(feedback)
-//
-//	w := httptest.NewRecorder()
-//	request, _ := http.NewRequest("POST", "/api/v1/feedback", strings.NewReader(string(feedbackJson)))
-//
-//	router.ServeHTTP(w, request)
-//
-//	fmt.Println(w.Body.String())
-//	assert.Equal(t, 400, w.Code)
-//}
+	feedbackController := controllers.FeedbackController{
+		Repository: repositories.FeedbackRepository{
+			Connection: databaseConnection,
+		},
+	}
+
+	router := gin.Default()
+	router.POST("/api/v1/feedback", feedbackController.Create)
+
+	feedback := models.Feedback{
+		Data: models.FeedbackData{
+			Title:   "test title",
+			Content: "test content",
+		},
+		Type:      1,
+		IsBug:     true,
+		AccountId: 1,
+	}
+
+	feedbackJson, _ := json.Marshal(feedback)
+
+	w := httptest.NewRecorder()
+	request, _ := http.NewRequest("POST", "/api/v1/feedback", strings.NewReader(string(feedbackJson)))
+	request.Header.Set("Content-Type", "application/json")
+
+	router.ServeHTTP(w, request)
+
+	assert.Equal(t, 201, w.Code)
+}
+
+func TestCreateFeedbackValidationError(t *testing.T) {
+	databaseConnection := database.Connection{
+		Driver: &drivers.PostgresDriver{},
+	}
+	databaseConnection.Connect()
+
+	feedbackController := controllers.FeedbackController{
+		Repository: repositories.FeedbackRepository{
+			Connection: databaseConnection,
+		},
+	}
+
+	router := gin.Default()
+	router.POST("/api/v1/feedback", feedbackController.Create)
+
+	feedback := models.Feedback{
+		Data: models.FeedbackData{
+			Title: "test title",
+		},
+		Type:      1,
+		IsBug:     true,
+		AccountId: 1,
+	}
+
+	feedbackJson, _ := json.Marshal(feedback)
+
+	w := httptest.NewRecorder()
+	request, _ := http.NewRequest("POST", "/api/v1/feedback", strings.NewReader(string(feedbackJson)))
+
+	router.ServeHTTP(w, request)
+
+	assert.Equal(t, 400, w.Code)
+}
 
 func TestGetFeedback(t *testing.T) {
-	databaseConnection := database.Connection{}
+	databaseConnection := database.Connection{
+		Driver: &drivers.PostgresDriver{},
+	}
 	databaseConnection.Connect()
 
 	feedbackController := controllers.FeedbackController{
@@ -85,7 +111,9 @@ func TestGetFeedback(t *testing.T) {
 }
 
 func TestGetFeedbackNotFound(t *testing.T) {
-	databaseConnection := database.Connection{}
+	databaseConnection := database.Connection{
+		Driver: &drivers.PostgresDriver{},
+	}
 	databaseConnection.Connect()
 
 	feedbackController := controllers.FeedbackController{
@@ -98,7 +126,7 @@ func TestGetFeedbackNotFound(t *testing.T) {
 	router.GET("/feedback/:id", feedbackController.GetById)
 
 	w := httptest.NewRecorder()
-	request, _ := http.NewRequest("GET", "/api/v1/feedback/1000", nil)
+	request, _ := http.NewRequest("GET", "/api/v1/feedback/10000", nil)
 	router.ServeHTTP(w, request)
 
 	assert.Equal(t, 404, w.Code)
