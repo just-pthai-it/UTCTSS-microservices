505e3a1dcef0f3669c84d3d6a23f6d3b93d1ebe7
just-pthai-it <pthai.it.210501@gmail.com>
2024-08-03 01:30:47 +0700

    perf(api): update database migrate tool.

--- database/migrations/tool/migrate.go
+++ database/migrations/tool/migrate.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "TSS-microservices/database"
+	"context"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/pressly/goose/v3"
 	"log"
@@ -9,17 +10,20 @@ import (
 )
 
 func main() {
-	MigrateDatabase("../" + os.Getenv("SERVICE_NAME"))
+	args := os.Args
+	command := args[2]
+	migrationsDirRelativePath := args[1]
+	MigrateDatabase(command, migrationsDirRelativePath+os.Getenv("SERVICE_NAME"))
 }
 
-func MigrateDatabase(migrationsFilepath string) {
+func MigrateDatabase(command string, migrationsDirRelativePath string) {
 	dataSourceString := "host=" + os.Getenv("DB_HOST") + " user=" + os.Getenv("DB_USERNAME") + " password=" + os.Getenv("DB_PASSWORD") + " dbname=" + os.Getenv("DB_NAME") + " port=" + os.Getenv("DB_PORT") + " sslmode=disable"
 	db, err := goose.OpenDBWithDriver(os.Getenv("DB_TYPE"), dataSourceString)
 	if err != nil {
 		log.Fatalf("failed to open DB: %v", err)
 	}
 
-	if err := goose.Up(db, migrationsFilepath); err != nil {
+	if err := goose.RunContext(context.Background(), command, db, migrationsDirRelativePath); err != nil {
 		log.Fatalf("failed to run migrations: %v", err)
 	}
 }
