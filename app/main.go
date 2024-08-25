c01bb2f5e54e428a83b4b7dcac40fb1a7fe67930
just-pthai-it <pthai.it.210501@gmail.com>
2024-08-26 00:30:22 +0700

    perf: add migrating migrations before running app.
    perf: add migrating migrations before running app.

--- app/main.go
+++ app/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"UTCTSS-microservices/common"
 	"UTCTSS-microservices/core/middlewares"
 	"UTCTSS-microservices/core/routes"
 	_ "UTCTSS-microservices/docs"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"log"
+	"os/exec"
+	"time"
 )
 
 func main() {
-	router := gin.Default()
+	migrateDatabase()
 
+	router := gin.Default()
 	router.Use(middlewares.ConsoleLoggerMiddleware)
 	router.Use(middlewares.FileLoggerMiddleware)
 	router.Use(middlewares.GinLoggerCustomFormat())
@@ -22,5 +27,30 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 		return
+	}
+}
+
+func migrateDatabase() {
+	databaseMigrateMaxAttempts := 10
+	for {
+		migrateCmd := exec.Command("./build/migrationtool", "./database/migrations/", "up")
+		output, err := migrateCmd.CombinedOutput()
+
+		if err != nil {
+			fmt.Println(common.Red + "Migrate failed: " + err.Error() + common.Reset)
+		} else {
+			fmt.Println(common.Blue + "Migrate successful!" + common.Reset)
+			fmt.Println(string(output))
+			break
+		}
+
+		databaseMigrateMaxAttempts--
+		if databaseMigrateMaxAttempts == 0 {
+			if err != nil {
+				log.Fatal(common.Red + "Exit because of database migrating failed!" + common.Reset)
+			}
+			break
+		}
+		time.Sleep(time.Second)
 	}
 }
